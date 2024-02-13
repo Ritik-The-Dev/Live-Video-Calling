780ee4e89825456cbbcf7c7987ae94dcf60b9a41
Ritik <joshiritik865@gmail.com>
2024-02-13 18:47:43 +0530

    brightness-Slider

--- client/src/components/VideoPlayer.jsx
+++ client/src/components/VideoPlayer.jsx
@@ -1,14 +1,28 @@
-import React, { useContext } from 'react';
-import { Grid, Typography, Paper, makeStyles } from '@material-ui/core';
+import React, { useContext, useState } from 'react';
+import { Grid, Typography, Paper, makeStyles, Slider } from '@material-ui/core';
 
 import { SocketContext } from '../Context';
 
 const useStyles = makeStyles((theme) => ({
+  videoContainer: {
+    display: 'inline-block',
+    position: 'relative',
+  },
   video: {
     width: '550px',
+    filter: 'brightness(100%)', // Initial brightness value
     [theme.breakpoints.down('xs')]: {
       width: '300px',
     },
+  },
+  slider: {
+    position: 'absolute',
+    width: '80%',
+    bottom: '0',
+    left: '0',
+    padding: '20px',
+    fontWeight: 'bold',
+    marginBottom:'-1.5rem'
   },
   gridContainer: {
     justifyContent: 'center',
@@ -26,6 +40,14 @@ const useStyles = makeStyles((theme) => ({
 const VideoPlayer = () => {
   const { name, callAccepted, myVideo, userVideo, callEnded, stream, call } = useContext(SocketContext);
   const classes = useStyles();
+  
+  // State variable to manage brightness value
+  const [brightness, setBrightness] = useState(110);
+
+  // Function to handle brightness change
+  const handleBrightnessChange = (event, newValue) => {
+    setBrightness(newValue);
+  };
 
   return (
     <Grid container className={classes.gridContainer}>
@@ -33,7 +55,19 @@ const VideoPlayer = () => {
         <Paper className={classes.paper}>
           <Grid item xs={12} md={6}>
             <Typography variant="h5" gutterBottom>{name || 'Name'}</Typography>
-            <video playsInline muted ref={myVideo} autoPlay className={classes.video} />
+            <div className={classes.videoContainer}>
+              <video playsInline muted ref={myVideo} autoPlay className={classes.video} style={{ filter: `brightness(${brightness}%)` }} />
+              {/* Slider to set brightness */}
+              <Slider
+                className={classes.slider}
+                value={brightness}
+                onChange={handleBrightnessChange}
+                min={100}
+                max={200}
+                step={1}
+                aria-labelledby="brightness-slider"
+              />
+            </div>
           </Grid>
         </Paper>
       )}
@@ -41,7 +75,19 @@ const VideoPlayer = () => {
         <Paper className={classes.paper}>
           <Grid item xs={12} md={6}>
             <Typography variant="h5" gutterBottom>{call.name || 'Name'}</Typography>
-            <video playsInline ref={userVideo} autoPlay className={classes.video} />
+            <div className={classes.videoContainer}>
+              <video playsInline ref={userVideo} autoPlay className={classes.video} style={{ filter: `brightness(${brightness}%)`, marginBottom:"2rem"}} />
+              {/* Slider to set brightness */}
+              <Slider
+                className={classes.slider}
+                value={brightness}
+                onChange={handleBrightnessChange}
+                min={100}
+                max={200}
+                step={1}
+                aria-labelledby="brightness-slider"
+              />
+            </div>
           </Grid>
         </Paper>
       )}
